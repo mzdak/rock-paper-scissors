0260089903db83583dfc2121a92530b9cabde121
mzdak <35873406+mzdak@users.noreply.github.com>
2023-01-05 15:02:54 +0530

    + Get user input (rock paper or scissor) + Add function to play a single round + Add function to loop through the rounds 5 times + Save the result of each round + Print the final winner (or draw).

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
 function getComputerChoice(){
-    let choice = Math.floor(Math.random() * 3) + 1;
+    let choice = Math.floor(Math.random() * 3) + 1;     //get get random number between 1 and 3.
+    //MDN says that this results in a no-uniform distributipn.Change later.
     console.log(choice)
+    return choice
 }
 
-getComputerChoice()
+function getUserChoice(){
+    let choice = prompt("Choose Rock Paper or Scissor: ").toLowerCase();
+    console.log(choice)
+    let choiceValue;
+    if (choice === "rock"){
+        choiceValue = 1;
+    }else if(choice === "paper"){
+        choiceValue = 2;
+    }else if(choice === "scissor"){
+        choiceValue = 3;
+    }else{
+        return "Wrong input."
+    }
+
+    return choiceValue;
+}
+
+function playRound(computerChoice, userChoice){
+    if(computerChoice===userChoice){
+        return "It's a draw!"
+    } else if(computerChoice===1 && userChoice===2 || computerChoice===2 && userChoice===3 || computerChoice===3 &&userChoice===1){
+        return "You win!"
+    } else{return "You lose..."}
+}
+
+function game(){
+    let userWin = 0
+    let compWin = 0
+    for(let i=1; i<= 5; i++){
+        let result = playRound(getComputerChoice(), getUserChoice())
+        console.log(result)
+        if (result === "You win!"){
+            userWin +=1
+        } else if(result === "You lose..."){
+            compWin +=1
+        }
+    }
+
+    if(userWin>compWin){
+        console.log("You win the game!")
+    } else if(compWin > userWin){
+        console.log("You lost the game...")
+    } else{ console.log("The game ended in a draw.")}
+}
+
+game()
