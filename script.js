e48ab1073ff3bf25170e95dc31c834c58b7d6124
mzdak <35873406+mzdak@users.noreply.github.com>
2023-01-20 18:06:21 +0530

    - polish the css - improve logic of game restart

--- script.js
+++ script.js
@@ -5,71 +5,76 @@ const result =document.querySelector('#result')
 const main =document.querySelector('#main')
 const roundResult = document.createElement('p');
 
-function getComputerChoice(){
-    let choice = Math.floor(Math.random() * 3) + 1;
-    if(choice==1) return 'rock';
-    if(choice==2) return 'paper';
-    if(choice==3) return 'scissors'
-}
-
 let userWin = 0;
 let compWin = 0;
 
-function playRound(computerChoice, userChoice){
-    console.log(computerChoice);
-    console.log(userChoice);
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 3) + 1;
+    if(choice==1){
+        return 'rock';
+    }
+    if(choice==2){ 
+        return 'paper';
+    }
+    if(choice==3) {
+        return 'scissors';
+    }
+    
+}
+
+function compareChoices(computerChoice, userChoice){
     if(computerChoice==='rock' && userChoice==='rock' ||
     computerChoice==='paper' && userChoice==='paper' ||
      computerChoice==='scissors' &&userChoice==='scissors'){
-        roundResult.innerHTML = `It's a draw.`
-        console.log("It's a draw!")
+        roundResult.innerHTML = `It's a draw.<br> You both chose ${userChoice.toUpperCase()}`
     } 
     else if(computerChoice==='rock' && userChoice==='paper'||
     computerChoice==='paper' && userChoice==='scissors'||
     computerChoice==='scissors' &&userChoice==='rock'){
-        roundResult.innerHTML = `You win. <br>${userChoice} beats ${computerChoice}.`
-        console.log("Paper beats rock. You win!");
+        roundResult.innerHTML = `You win. <br>${userChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}.`
         userWin +=1;
         userScore.textContent = `Player: ${userWin}`;
     }
     
     else {
-        roundResult.innerHTML = `You lose.<br> ${computerChoice} beats ${userChoice}.`
-        console.log("You lose...");
+        roundResult.innerHTML = `You lose.<br> ${computerChoice.toUpperCase()} beats ${userChoice.toUpperCase()}.`
         compWin += 1;
         compScore.textContent = `Computer: ${compWin}`;
     }
+}
 
+function gameOver(){
     if(userWin == 5){
-        console.log("You win the game!");
+        userScore.textContent = `Player: ${userWin}`;
+        compScore.textContent = `Computer: ${compWin}`;
         if(confirm("You've Won!\nDo you want to play again?")){
-            userWin = 0;
-            compWin = 0;
-            userScore.textContent = `Player: ${userWin}`;
-            compScore.textContent = `Computer: ${compWin}`;
-            roundResult.innerHTML = '';
+            location.reload()
         }
         
     }
     if(compWin == 5){
-        console.log("You lose the game!")
+        userScore.textContent = `Player: ${userWin}`;
+        compScore.textContent = `Computer: ${compWin}`;
         if(confirm('You Lose...\nDo you want to play again?')){
-            userWin = 0;
-            compWin = 0;
-            userScore.textContent = `Player: ${userWin}`;
-            compScore.textContent = `Computer: ${compWin}`;
-            roundResult.innerHTML = '';
+            location.reload()
         }
     }
+}
 
-    roundResult.setAttribute('style', 'display:flex; justify-content: center; align-items:center; font-size:1.5em');
+function playRound(computerChoice, userChoice){
+
+    compareChoices(computerChoice, userChoice);
+
+    roundResult.setAttribute(
+        'style', 'display:flex; justify-content: center; align-items:center; font-size:1.5em; text-align: center; font-weight: bold');
     main.appendChild(roundResult);
+
+    gameOver()
 }
 
 
 btn.forEach(button => {
-    button.addEventListener('click', function(){
-
-        playRound(getComputerChoice(), button.alt);
+    button.addEventListener('click', function(e){
+        playRound(getComputerChoice(), e.target.alt);
     });
 })
